7cd87cf3f849a03c1e18f11df4d6aba4ec54200d
zack-klein <klein.zachary.j@gmail.com>
2020-05-20 08:53:41 -0400

    style: flake8 and black

--- non_decreasing.py
+++ non_decreasing.py
@@ -4,12 +4,14 @@ https://leetcode.com/problems/non-decreasing-array/
 Needed to look at the solutions for this one.
 """
 
+
 # Brute force - Slooooow
 def checkPossibilityBruteForce(nums):
     """
     :type nums: List[int]
     :rtype: bool
     """
+
     def is_ascending(nums):
         for i, num in enumerate(nums):
             if i == len(nums) - 1:
@@ -21,7 +23,7 @@ def checkPossibilityBruteForce(nums):
     duplicate_list = nums[:]
 
     for i, num in enumerate(nums):
-        duplicate_list[i] = nums[i - 1] if i > 0 else float('-inf')
+        duplicate_list[i] = nums[i - 1] if i > 0 else float("-inf")
 
         if is_ascending(duplicate_list):
             return True
