b0cd587fde79b22fa51455f4e8dbaeb1bf61c23f
zack-klein <klein.zachary.j@gmail.com>
2020-05-24 09:13:36 -0400

    feat: add excel - couldn't solve

---
+++ excel_sheet_column_tile.py
@@ -0,0 +1,20 @@
+"""
+https://leetcode.com/problems/excel-sheet-column-title/
+
+Couldn't solve it, used this solution:
+https://leetcode.com/problems/excel-sheet-column-title/discuss/641281/Python-Using-string-lib
+"""
+import string
+
+
+def convertToTitle(n):
+    """
+    :type n: int
+    :rtype: str
+    """
+    letters = list(string.ascii_uppercase)
+    result = ""
+    while n != 0:
+        n, r = divmod(n-1,26)
+        result = letters[r] + result
+    return result
