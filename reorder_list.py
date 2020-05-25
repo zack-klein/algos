a3751f05bb24252df54bd43f83821a85d510e2d3
zack-klein <klein.zachary.j@gmail.com>
2020-05-25 10:47:06 -0400

    feat: add reorder list

---
+++ reorder_list.py
@@ -0,0 +1,50 @@
+"""
+https://leetcode.com/problems/reorder-list/submissions/
+"""
+import copy
+
+
+def reorderListBruteForce(self, head):
+    """
+        :type head: ListNode
+        :rtype: None Do not return anything, modify head in-place instead.
+        """
+    if not head:
+        return head
+
+    pos = 0
+    hash_table = {}
+    building_ht = True
+    node = head
+
+    while building_ht:
+
+        if not node.next:
+            building_ht = False
+
+        no_next_copy = copy.copy(node)
+        no_next_copy.copy = None
+        hash_table[pos] = no_next_copy
+
+        node = node.next
+        pos += 1
+
+    i = 0
+    counter = 0
+
+    current = head
+
+    while counter < len(hash_table) - 1:
+        left_new_pointer = hash_table[(len(hash_table) - 1) - i]
+        current.next = left_new_pointer
+        left_new_pointer.next = None
+        counter += 1
+
+        current = left_new_pointer
+        right_new_pointer = hash_table[i + 1]
+        current.next = right_new_pointer
+        right_new_pointer.next = None
+        counter += 1
+
+        i += 1
+        current = hash_table[i]
