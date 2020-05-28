934ea549b0ad559264f448565905ce5047620705
zack-klein <klein.zachary.j@gmail.com>
2020-05-28 07:55:00 -0400

    feat: add kth largest element

---
+++ kth_largest_element.py
@@ -0,0 +1,11 @@
+"""
+https://leetcode.com/problems/kth-largest-element-in-an-array/submissions/
+"""
+
+def findKthLargest(self, nums, k):
+    """
+    :type nums: List[int]
+    :type k: int
+    :rtype: int
+    """
+    return sorted(nums)[-k]
