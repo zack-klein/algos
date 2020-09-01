4de3113991e67e0f5d5bb26068622df252e2f3d3
zack-klein <klein.zachary.j@gmail.com>
2020-09-01 12:45:24 -0700

    feat: Add type-of-triangle

---
+++ type-of-triangle.sql
@@ -0,0 +1,13 @@
+-- https://www.hackerrank.com/challenges/what-type-of-triangle/problem
+
+select
+    case
+        when a = b and b = c
+        then 'Equilateral'
+        when a + b <= c or a + c <= b or b + c <= a
+        then 'Not A Triangle'
+        when a = b or a = c or b = c
+        then 'Isosceles'
+        else 'Scalene'
+    end as triangle
+from triangles;
