e0ca1fe8f2c3fe471b4c9de429edce787000e39a
zack-klein <klein.zachary.j@gmail.com>
2020-11-29 07:56:00 -0800

    feat: Use symbolic constants in farenheit converter

--- farenheit-celcius.c
+++ farenheit-celcius.c
@@ -1,31 +1,32 @@
 #include <stdio.h>
  /* print Fahrenheit-Celsius table
     for fahr = 0, 20, ..., 300 */
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
+
 main()
 {
   float fahr, celsius;
-  int lower, upper, step;
-  lower = 0;      /* lower limit of temperature table */
-  upper = 300;    /* upper limit */
-  step = 20;      /* step size */
-  fahr = lower;
+  fahr = LOWER;
   printf("Fahrenheit -> Celsius!\n");
   printf("----------------------\n");
-  while (fahr <= upper) {
+  while (fahr <= UPPER) {
     celsius = (5.0/9.0) * (fahr-32.0);
     printf("%3.0f\t%6.1f\n", fahr, celsius);
-    fahr = fahr + step;
+    fahr = fahr + STEP;
   }
 
   printf("\n\n");
 
-  celsius = lower;
+  celsius = LOWER;
   printf("Celsius -> Fahrenheit!\n");
   printf("----------------------\n");
-  while (celsius <= upper) {
+  while (celsius <= UPPER) {
     fahr = celsius * (9.0/5.0) + 32;
     printf("%3.0f\t%6.1f\n", celsius, fahr);
-    celsius = celsius + step;
+    celsius = celsius + STEP;
   }
 
 
