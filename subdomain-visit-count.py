697caa2dae55cfe5cd33461f79c6d40c1e8f9d34
zack-klein <klein.zachary.j@gmail.com>
2020-12-03 16:02:10 -0800

    feat: Add subdomain visit count

---
+++ subdomain-visit-count.py
@@ -0,0 +1,28 @@
+"""
+https://leetcode.com/problems/subdomain-visit-count/submissions/
+"""
+
+
+# This is pretty brute force... Worse case run time is O(n*m^2) where n =
+# number of elements in cpdomains and m is the maximum number of subdomains.
+def subdomainVisits(cpdomains):
+    hash_table = {}
+
+    for _domain in cpdomains:
+
+        count, domain = _domain.split(" ")
+        split_domain = domain.split(".")
+
+        for i in range(len(split_domain)):
+            subdomain = ".".join(split_domain[i:])
+
+            existing_count = hash_table.get(subdomain, 0)
+            new_count = existing_count + int(count)
+            hash_table[subdomain] = new_count
+
+    new_counts = []
+
+    for domain, count in hash_table.items():
+        new_counts.append(f"{count} {domain}")
+
+    return new_counts
