a74e131018dca39721f2ccb4418191b80c9d9e44
zack-klein <klein.zachary.j@gmail.com>
2020-12-05 13:41:23 -0800

    feat: Add move-zeroes with two pointers

--- move-zeroes.py
+++ move-zeroes.py
@@ -3,6 +3,7 @@ https://leetcode.com/problems/move-zeroes/
 """
 
 
+# Uses two arrays, concatenates, then reconstructs
 def moveZeroes(nums):
     """
     :type nums: List[int]
@@ -25,3 +26,33 @@ def moveZeroes(nums):
     for i, proper_num in enumerate(proper_order):
 
         nums[i] = proper_num
+
+
+# Uses two pointers
+def moveZeroesTwoPointers(self, nums):
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    first_zero = None
+
+    for i, num in enumerate(nums):
+        if num == 0:
+            first_zero = i
+            break
+
+    if first_zero is not None:
+
+        slow = first_zero
+        current = first_zero + 1
+
+        while current < len(nums):
+
+            if nums[current] != 0:
+                nums[slow] = nums[current]
+                nums[current] = 0
+
+                slow += 1
+
+            current += 1
+
+    return nums
