a468b66880862e836594d30d5b6e4ff317f513c7
zack-klein <klein.zachary.j@gmail.com>
2020-12-05 11:22:40 -0800

    feat: Add recursive reverse linked list

--- reverse-linked-list.py
+++ reverse-linked-list.py
@@ -11,3 +11,21 @@ def reverseList(head):
         current = temp_next
 
     return previous
+
+
+# Recursively
+def reverseListRecursive(head):
+
+    new_head = head  # noqa
+
+    def reverse(node):
+        if node and node.next:
+            reverse(node.next)
+            node.next.next = node
+            node.next = None
+        else:
+            new_head = node  # noqa
+
+    reverse(head)
+
+    return new_head
