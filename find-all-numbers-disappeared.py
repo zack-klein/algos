f1895d135db3e15cf11b4df5346720e42dcf9444
zack-klein <klein.zachary.j@gmail.com>
2020-12-07 14:45:26 -0500

    feat: Add constant space solution for missing numbers

--- find-all-numbers-disappeared.py
+++ find-all-numbers-disappeared.py
@@ -16,3 +16,21 @@ def findDisappearedNumbers(nums):
             missing.append(expected)
 
     return missing
+
+
+def findDisappearedNumbersConstantSpace(nums):
+
+    for i, num in enumerate(nums):
+        proper_index = abs(num) - 1
+        value_at_index = nums[proper_index]
+
+        if value_at_index > 0:
+            nums[proper_index] = value_at_index * -1
+
+    missing = []
+    for i, num in enumerate(nums):
+
+        if num > 0:
+            missing.append(i + 1)
+
+    return missing
