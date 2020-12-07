c05261c7da98d41cd470d6486c873b3b6e651033
zack-klein <klein.zachary.j@gmail.com>
2020-12-07 15:54:35 -0500

    feat: Add (gross) recursive solution for mergeTwoLists

--- merge-two-sorted-lists.py
+++ merge-two-sorted-lists.py
@@ -44,3 +44,82 @@ def mergeTwoLists(l1, l2):
         node.next = next_node
 
     return head
+
+
+# Done iteratively
+def mergeTwoListsIterative(l1, l2):
+    head = None
+    prev = None
+    L = l1
+    R = l2
+
+    while L or R:
+
+        if L and R:
+
+            if L.val <= R.val:
+                current = L
+                L = L.next
+
+            elif R.val < L.val:
+                current = R
+                R = R.next
+
+        elif L:
+            current = L
+            L = L.next
+
+        elif R:
+            current = R
+            R = R.next
+
+        if prev:
+            prev.next = current
+        else:
+            head = current
+
+        current.next = None
+        prev = current
+
+    return head
+
+
+# Aaand recursively!
+def mergeTwoListsRecursive(self, l1, l2):
+    self.head = None
+    self.prev = None
+
+    def merge(L, R):
+        if L or R:
+
+            if L and R:
+
+                if L.val <= R.val:
+                    current = L
+                    L = L.next
+
+                elif R.val < L.val:
+                    current = R
+                    R = R.next
+
+            elif L:
+                current = L
+                L = L.next
+
+            elif R:
+                current = R
+                R = R.next
+
+            if self.prev:
+                self.prev.next = current
+            else:
+                self.head = current
+
+            current.next = None
+            self.prev = current
+
+            merge(L, R)
+
+    merge(l1, l2)
+
+    return self.head
