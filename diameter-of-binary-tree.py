44e9146728fca04df2b00c883cfcd9a1d0f1e941
zack-klein <klein.zachary.j@gmail.com>
2020-12-12 09:39:23 -0500

    feat: Add redos of diameter of binary tree

--- diameter-of-binary-tree.py
+++ diameter-of-binary-tree.py
@@ -21,3 +21,46 @@ def diameterOfBinaryTree(self, root):
     dfs(root)
 
     return self.number_of_nodes - 1
+
+
+# After redoing this 4 times I finally got it.
+def diameterOfBinaryTreeRedo(self, root):
+    self.max = 1
+
+    def dfs(node):
+
+        if node:
+            L = dfs(node.left)
+            R = dfs(node.right)
+            self.max = max(self.max, L + R + 1)
+            return max(L, R) + 1
+
+        else:
+            return 0
+
+    dfs(root)
+
+    return self.max - 1
+
+
+# I'm a little upset that this one doesn't work. It passes the initial
+# test case but fails when we see a root node with no children. I'm sure
+# there's a way to get this one to work without having to do the magic
+# number nonsense we have to do above.
+def diameterOfBinaryTreeNoWork(self, root):
+    self.max = 1
+
+    def dfs(node):
+
+        if node:
+            L = dfs(node.left)
+            R = dfs(node.right)
+            self.max = max(self.max, L + R)
+            return max(L, R) + 1
+
+        else:
+            return 0
+
+    dfs(root)
+
+    return self.max
