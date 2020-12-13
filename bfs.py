5b42258849d69e1b38ee8a1406b9a6d58995d65f
zack-klein <klein.zachary.j@gmail.com>
2020-12-13 11:09:01 -0500

    feat: Use tree module in bfs

--- bfs.py
+++ bfs.py
@@ -63,87 +63,17 @@ def bfs_graph(root):
                 q.append(neighbor)
 
 
-# Binary Tree Test
-#      a
-#    /   \
-#   b     c
-#  / \   /  \
-# d   e f    g
-
+# Tests
 # Order of printing should be:
 # a, b, c, d, e, f, g
-
-
-class BinaryTreeNode:
-    def __init__(self, val, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-        self.visited = False
-
-    def __repr__(self):
-        return f"Node({self.val})"
-
-
-g = BinaryTreeNode("g")
-f = BinaryTreeNode("f")
-c = BinaryTreeNode("c")
-c.left = f
-c.right = g
-
-e = BinaryTreeNode("e")
-d = BinaryTreeNode("d")
-b = BinaryTreeNode("b")
-b.left = d
-b.right = e
-
-a = BinaryTreeNode("a")
-a.left = b
-a.right = c
+# For both
+from tests.trees import binary_tree, graph  # noqa
 
 print("\nBinary tree BFS:")
 print("-" * 20)
-bfs_binary_tree(a)
+bfs_binary_tree(binary_tree)
 
 
-# Arbitrary graph traversal test
-#   b - e   g       j
-#  /       /       /
-# a - c - f - h - i - k
-#  \               \
-#   d               l
-
-
-class GraphNode:
-    def __init__(self, val):
-        self.val = val
-        self.neighbors = []
-        self.visited = False
-
-    def __repr__(self):
-        return f"Node({self.val})"
-
-
-a = GraphNode("a")
-b = GraphNode("b")
-c = GraphNode("c")
-d = GraphNode("d")
-e = GraphNode("e")
-f = GraphNode("f")
-g = GraphNode("g")
-h = GraphNode("h")
-i = GraphNode("i")
-j = GraphNode("j")
-k = GraphNode("k")
-l = GraphNode("l")  # noqa
-
-i.neighbors += [j, k, l]
-h.neighbors.append(i)
-f.neighbors += [g, h]
-c.neighbors.append(f)
-b.neighbors.append(e)
-a.neighbors += [b, c, d]
-
-print("\n   Graph BFS:")
+print("\nGraph BFS:")
 print("-" * 20)
-bfs_graph(a)
+bfs_graph(graph)
